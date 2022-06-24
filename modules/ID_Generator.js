f75cda4c9c93d8e2039bcc2c2aff2d42c4d38d9b
Jibin Poulose <jibinp812@gmail.com>
2022-06-24 16:50:56 -0400

    Update ID_Generator.js

--- modules/ID_Generator.js
+++ modules/ID_Generator.js
@@ -1,14 +1,55 @@
-function userAutoModeSelectorID() {
-  var checkBox = document.getElementById("selectUserAutoID");
-  var text = document.getElementById("displayUserAutoID");
-  if (checkBox.checked == true){
-    console.log("Auto Mode");
-    displayUserAutoID.innerHTML = "Auto Mode Active";
-  } else {
-    console.log("User Mode");
-     displayUserAutoID.innerHTML = "User Mode Activte";
-  }
-  
+var modeSelectorID = "user";
+var myTransactionIDValue = "000";
 
-  
-}
+
+//window.onload = defaultModeForIDChecker();
+
+function defaultModeForIDChecker()
+		{
+    //console.log("User Mode");
+    displayUserAutoID.innerHTML = "User Mode Activte";
+		modeSelectorID = "user";
+		transactionIDUser();
+  	}
+
+
+function userAutoModeSelectorID() 
+	{
+	var checkBox = document.getElementById("selectUserAutoID");
+  var text = document.getElementById("displayUserAutoID");
+	
+	
+  if (checkBox.checked == true)
+		{
+    //console.log("Auto Mode");
+    displayUserAutoID.innerHTML = "Auto Mode Active";
+		modeSelectorID = "auto";
+		transactionIDAuto();
+  	} 
+	else 
+		{
+    defaultModeForIDChecker();
+  	}
+	}
+ 
+ function printMyTransactionIDValue()
+ 	{
+	document.getElementById('printTransactionID').innerHTML
+                ="Transaction Id : " + myTransactionIDValue;
+	}
+ 
+ function transactionIDUser()
+		{
+		
+		
+		
+		myTransactionIDValue = "00_User";
+		printMyTransactionIDValue();
+		}
+		
+		
+function transactionIDAuto()
+		{
+		myTransactionIDValue = "00_Auto";
+		printMyTransactionIDValue();
+		}
